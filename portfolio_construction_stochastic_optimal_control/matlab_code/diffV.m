55ccc161e21dc71649e5084a433cf9ff4cdb8c7f
Jose <joseleungbit@gmail.com>
2015-09-10 14:02:20 -0400

    Simplify the model to p=n

--- portfolio_construction_stochastic_optimal_control/matlab_code/diffV.m
+++ portfolio_construction_stochastic_optimal_control/matlab_code/diffV.m
@@ -2,9 +2,10 @@ function [b] = diffV(x)
 % Given x, asset price, output asset dynamics diff
 % Right now, I am hard coding dimension of dZ, 3, which is p dimensions in the paper
 % Return: diff matrix
+% Note: to get a dirty implementation, we let p=n at this point
 
   # mean reverting model
-  nVols = [13.2, 23.0, 21.1];  # This was hardcoded vol
+  nVols = [13.2, 23.0];        # This was hardcoded vol
   b = diag(nVols);             # b is the symbol in the paper
   
   # lognormal model

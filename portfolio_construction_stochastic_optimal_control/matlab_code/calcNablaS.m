9499392c47d1cd184c2451c00944860648f885c5
Jose <joseleungbit@gmail.com>
2015-09-14 20:20:36 -0400

    Added calcNablaS and optimalControlStrategy function

---
+++ portfolio_construction_stochastic_optimal_control/matlab_code/calcNablaS.m
@@ -0,0 +1,17 @@
+function [nablaS] = calcNablaS(x, t, T, timeStep, tol, maxIter)
+% Input: x: initial asset price vector, t: starting time, T: terminal time, timeStep: time interval between points, tol: tolerance, maxIter: maximum iteration times.
+% Output: gradient of S with respect to x vector
+% Note: make sure (T - t) / timeStep is even. 
+
+  F = length(x);
+  S = calcS(x, t, T, timeStep, tol, maxIter);
+  nablaS = zeros(F, 1);
+  eps = 1e-5;
+
+  for i = 1:F
+      xEps = x;
+      xEps(i) = xEps(i) + eps;
+      nablaS(i) = (calcS(xEps, t, T, timeStep, tol, maxIter) - S) / eps;
+  end
+
+end
---
+++ portfolio_construction_stochastic_optimal_control/matlab_code/optimalControlStrategy.m
@@ -0,0 +1,11 @@
+function [phi] = optimalControlStrategy(xCurr, tCurr, T, timeStep, tol, maxIter)
+% Input: xCurr: current asset price vector, tCurr: current time, T: terminal time, timeStep: time interval between points, tol: tolerance, maxIter: maximum iteration number
+% Output: phi: asset allocation weight vector
+% Note: make sure (T - tCurr) / timeStep is even, i.e. numStep is even.
+
+    invInst = invInstCov(xCurr)
+    drift = driftV(xCurr)
+    phi = invInstCov(xCurr) * driftV(xCurr) + calcNablaS(xCurr, tCurr, T, timeStep, tol, maxIter);
+end
+    
+

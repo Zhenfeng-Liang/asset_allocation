ff09dfeccdfa7bcea42b63a192987de23b55ce67
Jose <joseleungbit@gmail.com>
2015-10-27 21:25:29 -0400

    Prepare 10 asset test cases for penzias

--- portfolio_construction_stochastic_optimal_control/HJBWithConsumption/testScript.m
+++ portfolio_construction_stochastic_optimal_control/HJBWithConsumption/testScript.m
@@ -12,38 +12,17 @@ clear;
 
 % Optimize the mean reverting model
 %diary('log')
-%w0 = 100000;
-%utilityType = 'CRRA';
-%turnedOnConsumption = true;
-%numCores = 2;
-%modelParam.modelType = 'MeanReverting';
-%modelParam.mu = [0.4; 1.3; 2.2; 3.5; 1.2; 4.0; 5.5; 2.0; 1.0; 4.5];
-%modelParam.vol = [0.01; 0.016; 0.03; 0.052; 0.014; 0.05; 0.10; 0.03; 0.05; 0.08];
-%modelParam.lambda = [21.0; 13.2; 11.0; 12.4; 15.6; 6; 19; 23; 10.5; 8];
-%corrMatr = eye(10);
-%gamma = 10.0;
-%xCurr = [0.8; 0.8; 2; 4; 1; 3; 6; 1.5; 2.4; 5.1];
-%tCurr = 0;           
-%T = 1;               
-%timeStep = 0.01;
-%tol = 1e-6;
-%
-%strategy = wkbOptimizer(modelParam, corrMatr, gamma, xCurr, tCurr, ...
-%                        T, timeStep, tol, w0, utilityType, ...
-%                        turnedOnConsumption, numCores);
-%
-
 w0 = 100000;
 utilityType = 'CRRA';
 turnedOnConsumption = true;
-numCores = 2;
+numCores = 10;
 modelParam.modelType = 'MeanReverting';
-modelParam.mu = [0.4; 1.3];
-modelParam.vol = [0.01; 0.016];
-modelParam.lambda = [21.0; 13.2];
-corrMatr = eye(2.0);
+modelParam.mu = [0.4; 1.3; 2.2; 3.5; 1.2; 4.0; 5.5; 2.0; 1.0; 4.5];
+modelParam.vol = [0.01; 0.016; 0.03; 0.052; 0.014; 0.05; 0.10; 0.03; 0.05; 0.08];
+modelParam.lambda = [21.0; 13.2; 11.0; 12.4; 15.6; 6; 19; 23; 10.5; 8];
+corrMatr = eye(10);
 gamma = 10.0;
-xCurr = [0.8; 0.8];
+xCurr = [0.8; 0.8; 2; 4; 1; 3; 6; 1.5; 2.4; 5.1];
 tCurr = 0;           
 T = 1.0;               
 timeStep = 0.01;
@@ -53,6 +32,27 @@ strategy = wkbOptimizer(modelParam, corrMatr, gamma, xCurr, tCurr, ...
                         T, timeStep, tol, w0, utilityType, ...
                         turnedOnConsumption, numCores);
 
+
+%w0 = 100000;
+%utilityType = 'CRRA';
+%turnedOnConsumption = true;
+%numCores = 2;
+%modelParam.modelType = 'MeanReverting';
+%modelParam.mu = [0.4; 1.3];
+%modelParam.vol = [0.01; 0.016];
+%modelParam.lambda = [21.0; 13.2];
+%corrMatr = eye(2.0);
+%gamma = 10.0;
+%xCurr = [0.8; 0.8];
+%tCurr = 0;           
+%T = 1.0;               
+%timeStep = 0.01;
+%tol = 1e-6;
+%
+%strategy = wkbOptimizer(modelParam, corrMatr, gamma, xCurr, tCurr, ...
+%                        T, timeStep, tol, w0, utilityType, ...
+%                        turnedOnConsumption, numCores);
+%
 
 
 % Optimize the CIR model

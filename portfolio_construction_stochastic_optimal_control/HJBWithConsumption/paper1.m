3d4de6dddfdeaf3aaa514ab6572ac792cc2504ce
Jose <joseleungbit@gmail.com>
2015-11-09 16:14:55 -0500

    Changed CIR output graphs to be consistent with log normal

--- portfolio_construction_stochastic_optimal_control/HJBWithConsumption/paper1.m
+++ portfolio_construction_stochastic_optimal_control/HJBWithConsumption/paper1.m
@@ -5,8 +5,8 @@ function paper1()
     clear;
 
     %runLN();
-    runMR();
-    %runCIR();
+    %runMR();
+    runCIR();
     
 end
 
@@ -231,27 +231,144 @@ function runMR()
     figure;        
     for i = 1:numAsset
         subplot(numAsset,2,2*i-1);
-        plot(t, xFlow(i,:), 'Color', 'red');
+        plot(t, xFlow(i,:), 'Color', 'red', 'linewidth', 2);
         hold on;
-        plot(t, xFlowExact(i,:), 'Color', 'blue');
+        plot(t, xFlowExact(i,:), 'Color', 'blue', 'linewidth', 2);
         
         str1 = sprintf('Asset %d mean reverting wkb approximate xFlow and exact xFlow', i);
-        title(str1);
+        title(str1, 'FontSize', 20);
         
-        xlabel('time') % x-axis label
-        ylabel('xFlow') % y-axis label    
-        legend('Numerical','Exact');
+        xlabel('time', 'FontSize', 20) % x-axis label
+        ylabel('xFlow', 'FontSize', 20) % y-axis label    
+        h = legend('Numerical','Exact');
+        set(h, 'FontSize', 16);
         
         subplot(numAsset,2,2*i);
-        plot(t, pFlow(i,:), 'Color', 'red');
+        plot(t, pFlow(i,:), 'Color', 'red', 'linewidth', 2);
         hold on;
-        plot(t, pFlowExact(i,:), 'Color', 'blue');
+        plot(t, pFlowExact(i,:), 'Color', 'blue', 'linewidth', 2);
         
         str2 = sprintf('Asset %d mean reverting wkb approximate pFlow and exact pFlow', i);
-        title(str2);
-        xlabel('time') % x-axis label
-        ylabel('pFlow') % y-axis label        
-        legend('Numerical', 'Exact');
+        title(str2, 'FontSize', 20);
+        xlabel('time', 'FontSize', 20) % x-axis label
+        ylabel('pFlow', 'FontSize', 20) % y-axis label        
+        h = legend('Numerical', 'Exact');
+        set(h, 'FontSize', 16);
+    end
+    
+
+    btST = 0;           
+    btET = 0.05;               
+    rebTS = 0.01;
+
+    simulator = ModelEvolver();
+    simData = simulator.EvolveEuler(xCurr, btST, btET, rebTS, corrMatr, ...
+                                     model);
+
+    constr = Constraint(0.3, -0.2, false);
+    bte = BtEngine(btST, btET, rebTS, constr);
+
+    [wVec, phiMat, cVec] = bte.runBackTest(simData, wkbSolver, w0, ...
+                                           turnedOnConsumption);
+
+    t = btST:rebTS:btET;    
+    
+    % ploting strategy and simulated stock price
+    figure;
+    for i=1:numAsset
+        subplot(numAsset,1, i)
+        pos = [phiMat(i,:), 0];
+
+        [ax, h1, h2] = plotyy(t, simData(i,:), t, pos, 'plot', 'stairs');    
+        set(h1, 'linewidth', 2, 'color', 'red');
+        set(h2,  'linewidth', 2, 'color', 'blue');
+        
+        str = sprintf(['Asset %d simulated stock price and wkb ' ...
+                       'approximate position'],i);
+        title(str,'FontSize', 20);
+        
+        xlabel('rebalance time', 'FontSize', 20) % x-axis label
+        ylabel(ax(1), 'simulated stock price($)', 'color', 'red', 'FontSize', 20);
+        ylabel(ax(2), 'position', 'color', 'blue', 'FontSize', 20);
+        
+        h = legend('simulated stock price','wkb strategy', 'Location', 'southeast');
+        set(h, 'FontSize', 16);
+
+    end
+    
+    % Ploting PnL
+    figure;
+    plot(t, wVec(1,:), 'Color', 'red', 'linewidth', 2);    
+    title('Cumulative PnL, Mean Reverting', 'FontSize', 20);
+    xlabel('rebalance time', 'FontSize', 20); 
+    ylabel('PnL', 'FontSize', 20);  
+    
+    toc
+
+end
+
+
+function runCIR()
+
+    tic
+    
+    display('Running CIR');
+    w0 = 100000;
+    utilityType = 'HARA';
+    turnedOnConsumption = false;
+    numCores = 1;
+    modelParam.modelType = 'CIR';
+    modelParam.mu = [0.4; 1.3; 2.2; 3.5; 1.2; 4.0; 5.5; 2.0; 1.0; 4.5];
+    modelParam.vol = [0.1; 0.16; 0.3; 0.52; 0.14; 0.5; 1.0; 0.3; 0.5; 0.8];
+    modelParam.vol = modelParam.vol ./ sqrt(modelParam.mu); % For CIR
+    modelParam.lambda = [2.10; 1.32; 1.10; 1.24; 1.56; 0.6; 1.9; 2.3; 1.05; 0.8];
+    corrMatr = eye(10);
+
+    turnedOnConsumption = false;
+
+    gamma = 10.0;
+    xCurr = [0.8; 0.8; 2; 4; 1; 3; 6; 1.5; 2.4; 5.1];
+    pT = zeros(length(xCurr),1);
+        
+    tCurr = 0;
+    T = 1.0;
+    timeStep = 0.01;
+    tol = 1e-6;
+    
+
+    model = Model(modelParam);
+    portCalc = PortfolioCalculator(model, corrMatr);    
+    utiCalc = UtilityCalculator(gamma, utilityType);
+    hamSys = HamiltonianSystem(portCalc, utiCalc);       
+    wkbSolver = WKBHierarchySolver(hamSys, numCores);
+
+    % Ploting CIR numerical flow
+    xT = wkbSolver.solveForTermX(xCurr, tCurr, T, timeStep, tol);    
+    [xFlow, pFlow] = wkbSolver.generateLfFlow(xT, pT, tCurr, T, timeStep, tol);
+    
+    display('Plotting CIR leapfrog flow on the same graph.'); 
+    
+    F = length(xCurr);
+    numAsset = 2;
+    t = tCurr:timeStep:T; 
+    figure;        
+    for i = 1:numAsset
+        subplot(numAsset,2,2*i-1);
+        plot(t, xFlow(i,:), 'Color', 'red', 'linewidth', 2);
+        
+        str1 = sprintf('Asset %d CIR wkb approximate xFlow', i);
+        title(str1, 'FontSize', 20);
+        
+        xlabel('time', 'FontSize', 20) % x-axis label
+        ylabel('xFlow', 'FontSize', 20) % y-axis label    
+        
+        subplot(numAsset,2,2*i);
+        plot(t, pFlow(i,:), 'Color', 'red', 'linewidth', 2);
+        
+        str2 = sprintf('Asset %d CIR wkb approximate pFlow', i);
+        title(str2, 'FontSize', 20);
+        xlabel('time', 'FontSize', 20) % x-axis label
+        ylabel('pFlow', 'FontSize', 20) % y-axis label        
     end
     
 
@@ -296,65 +413,10 @@ function runMR()
     
     % Ploting PnL
     figure;
-    plot(t, wVec(1,:), 'Color', 'red');    
-    title('Cumulative PnL, Mean Reverting');
-    xlabel('rebalance time') 
-    ylabel('PnL')  
-    
-    toc
-
-end
-
-
-function runCIR()
-
-    tic
-    
-    display('Running CIR');
-    w0 = 100000;
-    utilityType = 'HARA';
-    turnedOnConsumption = false;
-    numCores = 1;
-    modelParam.modelType = 'CIR';
-    modelParam.mu = [0.4; 1.3; 2.2; 3.5; 1.2; 4.0; 5.5; 2.0; 1.0; 4.5];
-    modelParam.vol = [0.1; 0.16; 0.3; 0.52; 0.14; 0.5; 1.0; 0.3; ...
-                      0.5; 0.8];
-    modelParam.vol = modelParam.vol ./ sqrt(modelParam.mu);
-    
-    modelParam.lambda = [2.10; 1.32; 1.10; 1.24; 1.56; 0.6; 1.9; 2.3; 1.05; 0.8];
-    corrMatr = eye(10);
-    
-    gamma = 10.0;
-    xCurr = [0.8; 0.8; 2; 4; 1; 3; 6; 1.5; 2.4; 5.1];
-
-    turnedOnConsumption = false;
-        
-    btST = 0;           
-    btET = 0.1;               
-    rebTS = 0.01;
-
-    model = Model(modelParam);
-    portCalc = PortfolioCalculator(model, corrMatr);    
-    utiCalc = UtilityCalculator(gamma, utilityType);
-    hamSys = HamiltonianSystem(portCalc, utiCalc);       
-    wkbSolver = WKBHierarchySolver(hamSys, numCores);
-
-    simulator = ModelEvolver();
-    simData = simulator.EvolveEuler(xCurr, btST, btET, rebTS, corrMatr, ...
-                                     model);
-
-    constr = Constraint(0.3, -0.2, false);
-    bte = BtEngine(btST, btET, rebTS, constr);
-
-    [wVec, phiMat, cVec] = bte.runBackTest(simData, wkbSolver, w0, ...
-                                           turnedOnConsumption);
-
-    t = btST:rebTS:btET;
-    figure;
-    plot(t, wVec(1,:), 'Color', 'red');    
-    title('Cumulative PnL CIR');
-    xlabel('rebalance time') % x-axis label
-    ylabel('PnL') % y-axis label    
+    plot(t, wVec(1,:), 'Color', 'red', 'linewidth', 2);    
+    title('Cumulative PnL, CIR', 'FontSize', 20);
+    xlabel('rebalance time', 'FontSize', 20); 
+    ylabel('PnL', 'FontSize', 20);  
     
     toc
 

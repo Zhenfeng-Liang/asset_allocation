dcece37d82e7b18be5f16a68f7a5e47ee670cb87
Jose <joseleungbit@gmail.com>
2015-11-19 15:52:00 -0500

    Added monte carlo part code to run back-test

--- portfolio_construction_stochastic_optimal_control/HJBWithConsumption/paper1.m
+++ portfolio_construction_stochastic_optimal_control/HJBWithConsumption/paper1.m
@@ -1,10 +1,30 @@
 function paper1()
 
-    outdir = '11.17.LR';
+    outdir = '11.19.LR';
     %runLN(outdir);
     %runMR(outdir);
-    runCIR(outdir);
+    %runCIR(outdir);
+
+    numPaths = 100;
     
+    modelParam.modelType = 'MeanReverting';
+    modelParam.mu = [0.4; 1.3; 2.2; 3.5; 1.2; 4.0; 5.5; 2.0; 1.0; 4.5];
+    modelParam.vol = [0.1; 0.16; 0.3; 0.52; 0.14; 0.5; 1.0; 0.3; 0.5; 0.8];
+    modelParam.lambda = [2.10; 1.32; 1.10; 1.24; 1.56; 0.6; 1.9; 2.3; 1.05; 0.8];
+    xCurr = [0.8; 0.8; 2; 4; 1; 3; 6; 1.5; 2.4; 5.1];
+
+    runMonteCarlo(numPaths, modelParam, xCurr);
+
+
+    modelParam2.modelType = 'CIR';
+    modelParam2.mu = [0.4; 1.3; 2.2; 3.5; 1.2; 4.0; 5.5; 2.0; 1.0; 4.5];
+    modelParam2.vol = [0.1; 0.16; 0.3; 0.52; 0.14; 0.5; 1.0; 0.3; 0.5; 0.8];
+    modelParam2.vol = modelParam.vol ./ sqrt(modelParam.mu); 
+    modelParam2.lambda = [2.10; 1.32; 1.10; 1.24; 1.56; 0.6; 1.9; 2.3; 1.05; 0.8];
+    xCurr2 = [0.8; 0.8; 2; 4; 1; 3; 6; 1.5; 2.4; 5.1];
+
+    runMonteCarlo(numPaths, modelParam2, xCurr2);
+
 end
 
 
@@ -209,23 +229,17 @@ function runMR(outdir)
 
     F = length(modelParam.mu);
 
-%    d = -1;
-%    corrMatr = eye(F);
-%    while d <= 0
-%        a = 1 - rand(F) * 2;
-%        ata = a' * a;
-%        corrMatr = corrcov(ata);
-%        d = det(corrMatr);
-%    end
-%
-
+    % Generate correlation matrix
     tmp = randn(F, F);
     C = tmp * tmp';
     D = zeros(F, F);
     for i = 1:F
+        C(i, i) = 2 * C(i, i);
         D(i,i) = sqrt(C(i,i));
     end
     corrMatr = inv(D) * C * inv(D)
+    save('MeanRevertingcorr.mat', 'corrMatr');
+    
     rankofCorr = rank(corrMatr)
     eigenV = eig(corrMatr)
     inverseCorr = inv(corrMatr)
@@ -387,36 +401,16 @@ function runCIR(outdir)
     
     F = length(modelParam.mu);
 
-%    d = -1;
-%    feller = 0;
-%    corrMatr = eye(F);
-%    while feller < 1 
-%        a = 1 - rand(F) * 2;
-%        ata = a' * a;
-%               
-%        corrMatr = corrcov(ata);
-%        
-%        d = det(corrMatr);
-%        
-%        if(d <= 0)
-%            continue;
-%        end
-%        
-%        C = diag(modelParam.vol) * corrMatr * diag(modelParam.vol)';
-%        
-%        feller = 2 * modelParam.lambda' * (C \ modelParam.mu); 
-%    end    
-%    
-%    corrMatr
-%
     tmp = randn(F, F);
     C = tmp * tmp';
     D = zeros(F, F);
     for i = 1:F
+        C(i, i) = 2 * C(i, i);
         D(i,i) = sqrt(C(i,i));
     end
-    D
+    
     corrMatr = inv(D) * C * inv(D)
+    save('CIRcorr.mat', 'corrMatr');    
     rankofCorr = rank(corrMatr)
     eigenV = eig(corrMatr)
     inverseCorr = inv(corrMatr)
@@ -561,4 +555,142 @@ function [xFlow, pFlow] = calcExactMRFlow(sVec, A, T, y, m)
     xFlow = res(1:n, :);
     pFlow = res((n+1):2*n, :);
     
+end
+
+
+function runMonteCarlo(numPaths, modelParam, xCurr)
+
+    str = sprintf('Monte Carlo back test, %s', modelParam.modelType);
+    display(str);
+    
+    w0 = 100000;
+    utilityType = 'HARA';
+    turnedOnConsumption = false;
+    numCores = 1;
+    gamma = 10;
+    
+    btST = 0;           
+    btET = 1.0;               
+    rebTS = 0.02;
+
+    maxRet = 0.2;
+    maxDrawDown = -0.1;    
+    maxLR = 2.0;
+
+    % number of asset to plot the simulated stock price and pos
+    numAsset = 2;
+    
+    corrMatrFile = strcat(modelParam.modelType, 'corr.mat');
+    load(corrMatrFile);
+    
+    model = Model(modelParam);
+    portCalc = PortfolioCalculator(model, corrMatr);    
+    utiCalc = UtilityCalculator(gamma, utilityType);
+    hamSys = HamiltonianSystem(portCalc, utiCalc);       
+    wkbSolver = WKBHierarchySolver(hamSys, numCores);
+
+    wTVec = zeros(1, numPaths);
+    UTVec = zeros(1, numPaths);
+    
+    for path = 1:numPaths
+        
+        tic
+        str = sprintf('Running model %s path %d', modelParam.modelType, ...
+                      path);
+        display(str);
+        
+        simulator = ModelEvolver();
+        simData = simulator.EvolveEuler(xCurr, btST, btET, rebTS, corrMatr, ...
+                                        model);
+
+        constr = Constraint(maxRet, maxDrawDown, maxLR, true);
+        
+        bte = BtEngine(btST, btET, rebTS, constr);
+        
+        [wVec, phiMat, cVec] = bte.runBackTest(simData, wkbSolver, w0, ...
+                                               turnedOnConsumption);
+        
+        wTVec(path) = wVec(end);
+        UTVec(path) = utiCalc.U( wVec(end) );
+
+        % Start to plot the back-test result
+        t = btST:rebTS:btET;    
+        
+        % ploting strategy and simulated stock price
+        figure;
+        for i=1:numAsset
+            subplot(numAsset,1, i)
+            pos = [phiMat(i,:), 0];
+
+            [ax, h1, h2] = plotyy(t, simData(i,:), t, pos, 'plot', 'stairs');    
+            set(h1, 'linewidth', 2, 'color', 'red');
+            set(h2,  'linewidth', 2, 'color', 'blue');
+            
+            str = sprintf(['Asset %d simulated stock price and wkb ' ...
+                           'approximate position'],i);
+            title(str,'FontSize', 20);
+            
+            xlabel('rebalance time', 'FontSize', 20) % x-axis label
+            ylabel(ax(1), 'simulated stock price($)', 'color', 'red', 'FontSize', 20);
+            ylabel(ax(2), 'position', 'color', 'blue', 'FontSize', 20);
+            
+            h = legend('simulated stock price','wkb strategy', 'Location', 'southeast');
+            set(h, 'FontSize', 16);
+
+        end
+
+        str = sprintf('MC/%sPnPosPath%d.png', modelParam.modelType, ...
+                      path);
+        
+        set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 45 ...
+                            20]);
+        saveas(gcf, str);
+        
+        % Ploting PnL
+        figure;
+        plot(t, wVec(1,:), 'Color', 'red', 'linewidth', 2);
+        
+        str = sprintf('Cumulative PnL %s', modelParam.modelType);
+        title(str, 'FontSize', 20);
+        xlabel('rebalance time', 'FontSize', 20); 
+        ylabel('PnL', 'FontSize', 20);  
+
+        str = sprintf('MC/%sPnLPath%d.png', modelParam.modelType, path);
+        set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 45 ...
+                            20]);    
+        saveas(gcf, str);
+        
+        toc
+    
+    end
+    
+    averageWT = sum(wTVec) / numPaths
+    averageUT = sum(UTVec) / numPaths
+    
+    hist(wTVec)
+    str = sprintf('MC WT distribution %s', modelParam.modelType);
+    title(str, 'FontSize', 20);
+    xlabel('WT($)', 'FontSize', 20); 
+    ylabel('Frequency', 'FontSize', 20);  
+
+    str = sprintf('MC/%sWTHist.png', modelParam.modelType);
+    set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 45 20]);    
+    saveas(gcf, str);
+    
+    hist(UTVec)
+    str = sprintf('MC UT distribution %s', modelParam.modelType);
+    title(str, 'FontSize', 20);
+    xlabel('UT($)', 'FontSize', 20); 
+    ylabel('Frequency', 'FontSize', 20);  
+
+    str = sprintf('MC/%sUTHist.png', modelParam.modelType);
+    set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 45 20]);    
+    saveas(gcf, str);
+    
+    str = sprintf('MC/%sWTVec.mat', modelParam.modelType);
+    save(str, 'wTVec');
+
+    str = sprintf('MC/%sUTVec.mat', modelParam.modelType);    
+    save(str, 'UTVec');
+
 end
